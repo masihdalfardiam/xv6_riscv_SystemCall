0756b478f3d00d4c83e53d7f7a0d2ea91f608810
masih-dalfardi <58389776+masih-dalfardi@users.noreply.github.com>
2022-11-24 13:56:30 +0330

    Add files via upload

---
+++ user/kfreemem.c
@@ -0,0 +1,19 @@
+//
+// Created by masih on 11/24/22.
+//
+/*#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"*/
+
+#include "user.h"
+
+
+int
+
+main(int argc, char *argv[]) {
+
+    printf("xv6 free pages: %d\n");
+
+    exit(0);
+
+}
