c973d377108eb448608d08b1265cd78120782037
masih-dalfardi <58389776+masih-dalfardi@users.noreply.github.com>
2022-11-28 19:43:09 +0330

    Update kfreemem.c

--- user/kfreemem.c
+++ user/kfreemem.c
@@ -1,39 +1,8 @@
-//
-// Created by masih on 11/24/22.
-//
-/*#include "kernel/types.h"
-#include "kernel/stat.h"
-#include "user/user.h"*/
+#include "../kernel/types.h"
+#include "../kernel/stat.h"
+#include "../user/user.h"
 
-#include "user.h"
-
-
-int
-
-main(int argc, char *argv[]) {
-
-
-    int count = 0;
-    int mem = 0;
-    int n = 0;
-
-    struct run *r;
-
-    acquire(&kmem.lock);
-
-    r = kmem.freelist;
-
-    while (r != 0) {
-        count += 1;
-        r = r->next;
-    }
-    release(&kmem.lock);
-    n = getpagesize();
-    count = (n * count);
-    return count;
-    printf("xv6 free pages: %d\n");
-
+int main(int argc , char *argv[]){
+    printf("free memory = %d \n",kfreemem());
     exit(0);
-
-
 }
